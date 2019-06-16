1c04d3659e11a1c4555202661fe8dcdf69eced6d
AndreyKa986 <letum8658@tut.by>
2019-06-16 21:57:24 +0300

    dz0

---
+++ app/src/main/java/by/letum8658/homework/dz0/Dz0Activity.kt
@@ -0,0 +1,44 @@
+package by.letum8658.homework.dz0
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import by.letum8658.homework.R
+
+class Dz0Activity : Activity() {
+     private var variable = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dz0)
+
+        findViewById<View>(R.id.button)
+            .setOnClickListener { changeTextAndBackground() }
+
+        findViewById<View>(R.id.textViewOne)
+            .setOnClickListener { changeTextAndBackground() }
+
+        findViewById<View>(R.id.textViewTwo)
+            .setOnClickListener { changeTextAndBackground() }
+    }
+
+    @SuppressLint("ResourceAsColor", "NewApi")
+    private fun changeTextAndBackground() {
+        val textViewOne = findViewById<TextView>(R.id.textViewOne)
+        val textViewTwo = findViewById<TextView>(R.id.textViewTwo)
+        variable = if (variable == 0) {
+            textViewOne.text = getText(R.string.world)
+            textViewTwo.text = getText(R.string.hello)
+            textViewOne.setBackgroundColor(getColor(R.color.Blue))
+            textViewTwo.setBackgroundColor(getColor(R.color.Red))
+            1
+        }else{
+            textViewOne.text = getText(R.string.hello)
+            textViewTwo.text = getText(R.string.world)
+            textViewOne.setBackgroundColor(getColor(R.color.Red))
+            textViewTwo.setBackgroundColor(getColor(R.color.Blue))
+            0
+        }
+    }
+}
