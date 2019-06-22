ebed7bfe7c0b23f5a3c871ac58388a362892d96f
AndreyKa986 <letum8658@tut.by>
2019-06-22 15:52:57 +0300

    ClassWork was done

---
+++ app/src/main/java/by/letum8658/homework/dz2/Dz2ActivityClassWork.kt
@@ -0,0 +1,12 @@
+package by.letum8658.homework.dz2
+
+import android.app.Activity
+import android.os.Bundle
+import by.letum8658.homework.R
+
+class Dz2ActivityClassWork : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dz2_classwork)
+    }
+}
