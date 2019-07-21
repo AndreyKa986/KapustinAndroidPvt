b32ad1f504195ee5718443abe49d755c5e107b3c
AndreyKa986 <letum8658@tut.by>
2019-07-21 22:18:51 +0300

    Dz11 was done

--- app/src/main/java/by/letum8658/homework/dz11/Dz11CarActivity.kt
+++ app/src/main/java/by/letum8658/homework/dz11/Dz11CarActivity.kt
@@ -1,17 +1,129 @@
 package by.letum8658.homework.dz11
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import by.letum8658.homework.R
+import by.letum8658.homework.dz9.entity.CoordParams
+import by.letum8658.homework.dz9.entity.Coordinate
+import by.letum8658.homework.dz9.entity.Poi
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_dz9.*
 
-class Dz11CarActivity : FragmentActivity() {
+class Dz11CarActivity : FragmentActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<FrameLayout>
+    private lateinit var poiPicture: BitmapDescriptor
+    private var isOnePoiOnScreenNow = false
+    private lateinit var viewModel: Dz11CarViewModel
+
+    companion object {
+        const val PADDING = 100
+        const val ZOOM = 18f
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dz9)
 
+        viewModel = ViewModelProviders.of(this).get(Dz11CarViewModel::class.java)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.dz9BottomSheetContainer, Dz11CarFragment())
+                .commit()
+        }
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.dz9mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        bottomSheetBehavior = BottomSheetBehavior.from(dz9BottomSheetContainer)
+
+        poiPicture = bitmapDescriptorFromVector(this, R.drawable.ic_car)!!
+
+        val params = CoordParams(Coordinate(0.0, 0.0), Coordinate(0.0, 0.0))
+
+        viewModel
+            .state
+            .observe(this, Observer {
+                when (it) {
+                    is Dz11CarState.AllData -> {
+                        showPoi(it.poiList)
+                    }
+                    is Dz11CarState.OnePoiOfData -> {
+                        onPoiClick(it.poi)
+                    }
+                    is Dz11CarState.Error -> {
+                        Toast.makeText(this, getText(R.string.sorry2), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
+
+        viewModel.loadPoiList(params)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+    }
+
+    private fun showPoi(poiList: List<Poi>) {
+        var place: LatLng
+        val builder = LatLngBounds.Builder()
+        map.clear()
+        poiList.forEach {
+            place = LatLng(it.coordinate!!.latitude, it.coordinate.longitude)
+            map.addMarker(
+                MarkerOptions()
+                    .position(place)
+                    .icon(poiPicture)
+                    .rotation(it.heading!!.toFloat())
+            )
+            builder.include(place)
+        }
+        val bounds = builder.build()
+        map.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, PADDING))
+        isOnePoiOnScreenNow = false
+    }
+
+    private fun onPoiClick(poi: Poi) {
+        val poiPlace = LatLng(poi.coordinate!!.latitude, poi.coordinate.longitude)
+        map.clear()
+        map.addMarker(
+            MarkerOptions()
+                .position(poiPlace)
+                .icon(poiPicture)
+                .rotation(poi.heading!!.toFloat())
+        )
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(poiPlace, ZOOM))
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        isOnePoiOnScreenNow = true
+    }
+
+    private fun bitmapDescriptorFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
+        return ContextCompat.getDrawable(context, vectorResId)?.run {
+            setBounds(0, 0, intrinsicWidth, intrinsicHeight)
+            val bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
+            draw(Canvas(bitmap))
+            BitmapDescriptorFactory.fromBitmap(bitmap)
         }
     }
+
+    override fun onBackPressed() = if (isOnePoiOnScreenNow) showPoi(viewModel.getPoiList()) else super.onBackPressed()
 }
--- app/src/main/java/by/letum8658/homework/dz11/Dz11CarFragment.kt
+++ app/src/main/java/by/letum8658/homework/dz11/Dz11CarFragment.kt
@@ -5,10 +5,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import by.letum8658.homework.R
+import by.letum8658.homework.dz9.Dz9Adapter
+import by.letum8658.homework.dz9.entity.Poi
 
-class Dz11CarFragment : Fragment() {
+class Dz11CarFragment : Fragment(), Dz9Adapter.ClickListener {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewModel: Dz11CarViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.frafment_bottom_sheet_dz9, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        recyclerView = view.findViewById(R.id.dz9RecyclerView)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        viewModel = ViewModelProviders.of(activity!!).get(Dz11CarViewModel::class.java)
+
+        viewModel
+            .state
+            .observe(this, Observer {
+                when (it) {
+                    is Dz11CarState.AllData -> {
+                        recyclerView.adapter = Dz9Adapter(it.poiList, this)
+                    }
+                }
+            })
+    }
+
+    override fun onPoiClick(item: Poi) {
+        viewModel.setPoi(item)
     }
 }
